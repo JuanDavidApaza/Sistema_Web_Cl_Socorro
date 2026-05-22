d552e50647db06393bf10be9d74bd381eb0eac1f
STEWARD <STEWARD@DESKTOP-092A8NP>
2026-05-21 23:57:28 -0500

    Agregado módulo de contacto completado para el Sprint 1

---
+++ src/main/java/com/clinicasocorro/ContactoController.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.clinicasocorro; // Asegúrate de que este sea tu paquete exacto
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ContactoController {
+    
+    @GetMapping("/contacto")
+    public String mostrarContacto() {
+        return "contacto"; 
+    }
+}
